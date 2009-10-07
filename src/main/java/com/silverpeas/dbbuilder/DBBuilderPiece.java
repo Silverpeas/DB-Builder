a5dbe6faef111568295c89356f04914e4dc26181
Emmanuel Hugonnet <emmanuel.hugonnet@silverpeas.com>
2009-10-07 08:54:08 +0000

    Reformatting code
    git-svn-id: https://www.silverpeas.org/svn/silverpeas/services/db-builder/trunk@202 a8e77078-a1c7-4fa5-b8fc-53c5178a176c

--- src/main/java/com/silverpeas/dbbuilder/DBBuilderPiece.java
+++ src/main/java/com/silverpeas/dbbuilder/DBBuilderPiece.java
@@ -1,17 +1,25 @@
 /**
- * Titre :        dbBuilderPiece
- * Description :  Superclasse des pièces de contruction de la base de données.
- * Une pièce de construction est un fichier ou une chaîne de caractères contenant une séquence
- * exécutable sur un serveur de données.
- * Cette superclasse offre les services suivants :
- * - chargement du fichier si la pièce est de type fichier
- * - déclaration d'une classe abstraite de transformation de la chaîbe en liste d'instructions
- * interprétables et exécutables par le serveur de données
- * exécution de la séquence d'instructions en relation avec la classe connecteur JDBC com.stratelia.dbConnector.DBConnexion
- * Copyright :    Copyright (c) 2001 - 2002
- * Société :      Stratélia Silverpeas
- * @author ATH
- * @version 1.0
+ * Copyright (C) 2000 - 2009 Silverpeas
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * As a special exception to the terms and conditions of version 3.0 of
+ * the GPL, you may redistribute this Program in connection with Free/Libre
+ * Open Source Software ("FLOSS") applications as described in Silverpeas's
+ * FLOSS exception.  You should have recieved a copy of the text describing
+ * the FLOSS exception, and it is also available here:
+ * "http://repository.silverpeas.com/legal/licensing"
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.silverpeas.dbbuilder;
 
